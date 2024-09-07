49b98a8b7691c8e0be3bc257462039e05dacda4d
Niyantha M <maruthup@usc.edu>
2024-09-07 13:25:37 -0700

    Implemented Encoder skeleton

--- model.py
+++ model.py
@@ -78,6 +78,33 @@ class RMSNorm(nn.Module):
         return self.weight * self._norm(x.float()).type_as(x)
 
 
+class EncoderBlock(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForward(args)
+
+        # Normalize Before self attention
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+        # Normalize before feed forward
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+    def forward(
+        self, x: torch.Tensor, start_pos: int, freqs_complex: torch.Tensor
+    ):
+        h = x + self.attention.forward(
+            self.attention_norm(x), start_pos, freqs_complex
+        )
+        out = h + self.feed_forward.forward(self.ffn_norm(h))
+        return out
+
+
 class Transformer(nn.Module):
 
     def __init__(self, args: ModelArgs) -> None:
