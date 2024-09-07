c848c3744b9923ea58546439df81458069d9dfc7
Niyantha M <maruthup@usc.edu>
2024-09-07 12:59:32 -0700

    Implemented RMSNorm

--- model.py
+++ model.py
@@ -62,6 +62,22 @@ def apply_rotary_embeddings(
     return x_out.type_as(x).to(device)
 
 
+class RMSNorm(nn.Module):
+
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x: torch.Tensor):
+
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x: torch.Tensor):
+
+        return self.weight * self._norm(x.float()).type_as(x)
+
+
 class Transformer(nn.Module):
 
     def __init__(self, args: ModelArgs) -> None:
