636ebdd018152290ff72ed57532e7453225f6010
Niyantha M <maruthup@usc.edu>
2024-09-09 20:30:37 -0700

    Implemented FeedForward

--- model.py
+++ model.py
@@ -179,7 +179,30 @@ class SelfAttention(nn.Module):
         return self.wo(output)
 
 
-# class FeedForward(nn.Module):
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2 * hidden_dim / 3)
+
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
+
+        # round hidden dim to nearest numtilple_of
+        hidden = args.multiple_of * (
+            (hidden + args.multiple_of - 1) // args.multiple_of
+        )
+
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        swish = F.silu(self.w1(x))
+        x_v = self.w3(x)
+        x = swish * x_v
+        x = self.w2(x)
+        return x
 
 
 class EncoderBlock(nn.Module):
