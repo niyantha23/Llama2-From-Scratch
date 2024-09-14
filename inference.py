fabe251e85fe04b0e78666a2b9c5add729bdb16e
Niyantha M <maruthup@usc.edu>
2024-09-13 23:19:47 -0700

    implemented top_p

--- inference.py
+++ inference.py
@@ -68,7 +68,7 @@ class LLaMA:
             #populate with prompt tokens
             tokens[k,:len(t)]=torch.tensor(t,dtype=torch.long,device=device)
         
-        eos_reached=torch.Tensor([False]*batch_size,device=device)
+        eos_reached=torch.tensor([False]*batch_size,device=device)
         prompt_tokens_mask=tokens !=pad_id
         for curr_pos in tqdm(range(1,total_len),desc='Generating Tokens'):
             with torch.no_grad():
@@ -100,14 +100,51 @@ class LLaMA:
             out_text.append(self.tokenizer.decode(curr_prompt_tokens))
         return (out_tokens,out_text)
 
+    def _sample_top_p(self,probs,p):
+        probs_sort,probs_idx=torch.sort(probs,dim=-1,descending=True)
+        probs_sum=torch.cumsum(probs_sort,dim=-1)
+        mask=probs_sum-probs_sort>p
+        probs_sort[mask]=0.0
+        probs_sort.div_(probs_sort.sum(dim=-1,keepdim=True))
+        next_token=torch.multinomial(probs_sort,num_samples=1)
+        next_token=torch.gather(probs_idx,-1,next_token)
+        return next_token
 
 
 if __name__=='__main__':
     torch.manual_seed(0)
 
-    allow_cuda=False
-    device='cuda' if torch.cuda.is_available() and allow_cuda else 'cpu'
+    allow_cuda = False
+    device = 'cuda' if torch.cuda.is_available() and allow_cuda else 'cpu'
 
-    model=LLaMA.build(checkpoints_dir='llama-2-7b/',tokenizer_path='tokenizer.model',load_model=True,max_seq_len=1024,max_batch_size=3,device=device)
+    prompts = [
+        "Simply put, the theory of relativity states that ",
+        "If Google was an Italian company founded in Milan, it would",
+        # Few shot promt
+        """Translate English to French:
+        
+        sea otter => loutre de mer
+        peppermint => menthe poivrée
+        plush girafe => girafe peluche
+        cheese =>""",
+        # Zero shot prompt
+        """Tell me if the following person is actually Doraemon disguised as human:
+        Name: Umar Jamil
+        Decision: 
+        """
+    ]
 
-    print('All Ok')
+    model = LLaMA.build(
+        checkpoints_dir='llama-2-7b/',
+        tokenizer_path='tokenizer.model',
+        load_model=True,
+        max_seq_len=1024,
+        max_batch_size=len(prompts),
+        device=device
+    )
+
+    out_tokens, out_texts = (model.text_completion(prompts, max_gen_len=64))
+    assert len(out_texts) == len(prompts)
+    for i in range(len(out_texts)):
+        print(f'{out_texts[i]}')
+        print('-' * 50)
